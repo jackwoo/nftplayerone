f5f119bb351328a4e1bb72d7055a2e172b3b3067
Dongfeng <xdf@kandaovr.com>
2021-12-01 19:54:33 +0800

    add sample chrome plugin

---
+++ chrome/background/index.js
@@ -0,0 +1,32 @@
+// Called when the user clicks on the browser action.
+console.log(chrome.browserAction)
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     // Send a message to the active tab
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//         var activeTab = tabs[0];
+//         chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+//     });
+// });
+
+// This block is new!
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "open_new_tab" ) {
+            chrome.tabs.create({"url": request.url});
+        }
+    }
+);
+
+// chrome.runtime.onInstalled.addListener(function(){
+// 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+// 		chrome.declarativeContent.onPageChanged.addRules([
+// 			{
+// 				conditions: [
+// 					// 只有打开百度才显示pageAction
+// 					new chrome.declarativeContent.PageStateMatcher({pageUrl: {urlContains: 'baidu.com'}})
+// 				],
+// 				actions: [new chrome.declarativeContent.ShowPageAction()]
+// 			}
+// 		]);
+// 	});
+// });
